072ffb00ae459b464613f764dc4334dcda573d23
Vineeth T R <Vineeth.TR@gds.ey.com>
2021-07-22 19:49:25 +0530

    mt3

--- js/loaders/line.js
+++ js/loaders/line.js
@@ -119,7 +119,9 @@ export const LINE = [
   }
 }
     `,
-    }, {
+    },
+    
+    {
         id: "dkndou5oj6o",
         html: `<span class="loader"></span>`,
         css: `.loader {
@@ -151,7 +153,31 @@ export const LINE = [
   }
 }
     `,
-    }, {
+    },
+
+    {
+        id: "nd6o-mtrix",
+        html: `<span class="loader"></span>`,
+        css: `.loader {
+          width:45px;
+          height:40px;
+          background:
+            linear-gradient(#0000 calc(1*100%/6),#fff 0 calc(3*100%/6),#0000 0),
+            linear-gradient(#0000 calc(2*100%/6),#fff 0 calc(4*100%/6),#0000 0),
+            linear-gradient(#0000 calc(3*100%/6),#fff 0 calc(5*100%/6),#0000 0);
+          background-size:10px 400%;
+          background-repeat: no-repeat;
+          animation:matrix 1s infinite linear;
+          }
+          @keyframes matrix {
+            0% {background-position: 0% 100%, 50% 100%, 100% 100% }
+            100% {background-position: 0% 0%, 50% 0%, 100% 0% }
+          }
+    `,
+    },
+    
+    
+    {
         id: "xr1ogquld0d",
         html: `<span class="loader"></span>`,
         css: `.loader {
