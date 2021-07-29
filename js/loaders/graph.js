6167d0458259b48c891ecd15595b9eb7c0dec90a
Vineeth T R <Vineeth.TR@gds.ey.com>
2021-07-29 23:37:51 +0530

    Toast

--- js/loaders/graph.js
+++ js/loaders/graph.js
@@ -195,6 +195,101 @@ background-image: linear-gradient(0deg,
           }
 `,
     },
+  {
+    id: "imgSunMuntRase2",
+    html: `<span class="loader"></span>`,
+    css: `.loader {
+      width: 64px;
+      height: 64px;
+      position: relative;
+      background: #FFF;
+      border-radius: 4px;
+      overflow: hidden;
+    }
+    .loader:before {
+      content: "";
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      width: 40px;
+      height: 40px;
+      transform: rotate(45deg) translate(30%, 40%);
+      background: #ff9371;
+      box-shadow: 32px -34px 0 5px #ff3d00;
+      animation: slide 2s infinite ease-in-out alternate;
+    }
+    .loader:after {
+      content: "";
+      position: absolute;
+      left: 10px;
+      top: 10px;
+      width: 16px;
+      height: 16px;
+      border-radius: 50%;
+      background: #ff3d00;
+      transform: rotate(0deg);
+      transform-origin: 35px 145px;
+      animation: rotate 2s infinite ease-in-out;
+    }
+
+    @keyframes slide {
+      0% , 100%{ bottom: -35px}
+      25% , 75%{ bottom: -2px}
+      20% , 80%{ bottom: 2px}
+    }
+    @keyframes rotate {
+      0% { transform: rotate(-15deg)}
+      25% , 75%{ transform: rotate(0deg)}
+      100% {  transform: rotate(25deg)}
+    }
+  `,
+    },
+
+  {
+    id: "navigator2SpinLding",
+    html: `<span class="loader"></span>`,
+    css: `.loader {
+      width: 64px;
+      height: 64px;
+      position: relative;
+      background: #FFF;
+      border-radius: 50%;
+      transform: rotate(45deg);
+      animation: rotate 2s linear infinite;
+    }
+    .loader:before {
+      content: "";
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      width: 15px;
+      height: 30px;
+      background: #FF3D00;
+      transform: skew(5deg , 60deg) translate(-50%, -5%);
+    }
+
+    .loader:after {
+      content: "";
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      width: 6px;
+      height: 6px;
+      border-radius: 50%;
+      background: #FFF;
+      transform:translate(-50% , -50%);
+    }
+
+    @keyframes rotate {
+      0% { transform: rotate(45deg)}
+      30% , 50% , 70%  { transform: rotate(230deg)}
+      40% , 60% , 80% { transform: rotate(240deg)}
+      100% {  transform: rotate(245deg)}
+   }
+  `,
+    },
+
+
     {
         id: "eyeBallMoveSinleo6e",
         html: `<span class="loader"></span>`,
--- js/loaders/rect.js
+++ js/loaders/rect.js
@@ -559,6 +559,42 @@ export const RECT = [
 }
     `,
   },
+  {
+    id: "sqr4xmlt2clrRotSpn",
+    html: `<span class="loader"></span>`,
+    css: `.loader {
+        width: 64px;
+        height: 64px;
+        position: relative;
+        background-image:
+          linear-gradient(#FFF 16px, transparent 0) ,
+          linear-gradient(#FF3D00 16px, transparent 0) ,
+          linear-gradient(#FF3D00 16px, transparent 0) ,
+          linear-gradient(#FFF 16px, transparent 0);
+        background-repeat: no-repeat;
+        background-size: 16px 16px;
+        background-position: left top , left bottom , right top , right bottom;
+        animation: rotate 1s linear infinite;
+      }
+      @keyframes rotate {
+        0% {
+          width: 64px;
+          height: 64px;
+          transform: rotate(0deg)
+        }
+        50% {
+          width: 30px;
+          height: 30px;
+          transform: rotate(180deg)
+        }
+        100% {
+          width: 64px;
+          height: 64px;
+          transform: rotate(360deg)
+        }
+      }
+    `,
+  },
 
   {
     id: "sqrFlwupShdw67",
