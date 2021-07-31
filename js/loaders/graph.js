232182fd1190d796c2fa63289c05912dc4e28271
Vineeth T R <Vineeth.TR@gds.ey.com>
2021-07-31 14:26:19 +0530

    crl 201

--- js/loaders/graph.js
+++ js/loaders/graph.js
@@ -196,6 +196,50 @@ background-image: linear-gradient(0deg,
 `,
     },
   {
+    id: "packManEt2Xb",
+    html: `<span class="loader"></span>`,
+    css: `.loader {
+        position: relative;
+        border: 24px solid #FFF;
+        border-radius: 50%;
+        box-sizing: border-box;
+        animation: eat 1s linear infinite;
+      }
+      .loader::after , .loader::before {
+        content: '';
+        position: absolute;
+        left: 50px;
+        top: 50%;
+        transform: translateY(-50%);
+        background: #fff;
+        width: 15px;
+        height: 15px;
+        border-radius: 50%;
+        box-sizing: border-box;
+        opacity: 0;
+        animation: move 2s linear infinite;
+      }
+
+      .loader::before {
+        animation-delay: 1s;
+      }
+
+      @keyframes eat {
+        0% , 49% { border-right-color: #FFF }
+        50% , 100% { border-right-color: #0000 }
+      }
+      @keyframes move {
+        0% { left: 75px ; opacity: 1}
+        50% { left: 0px; opacity: 1 }
+        52%, 100% { left: -5px; opacity: 0; }
+      }
+`,
+    },
+
+
+
+
+  {
     id: "imgSunMuntRase2",
     html: `<span class="loader"></span>`,
     css: `.loader {
