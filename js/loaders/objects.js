42b9337771f5d03c65ce90d5dd234dbe8326fa2a
Vineeth T R <Vineeth.TR@gds.ey.com>
2021-10-31 10:30:30 +0530

    perspectie fix

--- js/loaders/objects.js
+++ js/loaders/objects.js
@@ -1,5 +1,37 @@
 export const OBJECTS = [
   {
+    id: "stackLyrXp",
+    html: `<span class="loader"></span>`,
+    css: `.loader {
+    position: relative;
+    width:  48px;
+    height: 48px;
+    background: #de3500;
+    transform: rotateX(65deg) rotate(45deg);
+    // remove bellows command for perspective change
+    //transform: perspective(200px) rotateX(65deg) rotate(45deg); 
+    color: #fff;
+    animation: layers1 1s linear infinite alternate;
+  }
+  .loader:after {
+    content: '';
+    position: absolute;
+    inset: 0;
+    background: rgba(255, 255, 255, 0.7);
+    animation: layerTr 1s linear infinite alternate;
+  }
+
+  @keyframes layers1 {
+    0% { box-shadow: 0px 0px 0 0px  }
+   90% , 100% { box-shadow: 20px 20px 0 -4px  }
+  }
+  @keyframes layerTr {
+    0% { transform:  translate(0, 0) scale(1) }
+    100% {  transform: translate(-25px, -25px) scale(1) }
+  }
+      `,
+  },
+  {
     id: "sqrBxRol",
     html: `<span class="loader"></span>`,
     css: `.loader {
@@ -111,37 +143,7 @@ export const OBJECTS = [
 }
       `,
   },
-  {
-    id: "layerSprd",
-    html: `<span class="loader"></span>`,
-    css: `.loader {
-    position: relative;
-    width:  48px;
-    height: 48px;
-    background: #de3500;
-    transform: perspective(200px) rotateX(65deg) rotate(45deg);
-    color: #fff;
-    animation: layers1 1s linear infinite alternate;
-  }
-  .loader:after {
-    content: '';
-    position: absolute;
-    inset: 0;
-    background: rgba(255, 255, 255, 0.7);
-    animation: layers2 1s linear infinite alternate;
-  }
 
-  @keyframes layers1 {
-    0% { box-shadow: 0px 0px 0 0px  }
-   90% , 100% { box-shadow: 20px 20px 0 -4px  }
-  }
-
-  @keyframes layers2 {
-    0% { transform: translate(0, 0) scale(1) }
-    90% , 100% { transform: translate(-25px, -25px) scale(1.2) }
-  }
-      `,
-  },
   {
     id: "brckBblUp",
     html: `<span class="loader"></span>`,
